da251b2f35c6d049943560b3ad71a0d92bf524f4
margot <margot@thinkr.fr>
2022-01-11 14:42:19 +0100

    Try a new gitlab template to deploy on Connect through CI/CD
    Why:
    
    * To use CI/CD
    
    This change addresses the need by:
    
    * Modifying dev/dev_history_connect.R
    * Modifying gitlabci template
    
    Issues
    Issue #7

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # finddxtemplate
 
+<!-- badges: start -->
+
+[![pipeline
+status](https://forge.thinkr.fr/%3Cgroup%3E/%3Cproject%3E/badges/master/pipeline.svg)](https://forge.thinkr.fr/%3Cgroup%3E/%3Cproject%3E/-/commits/master)
+<!-- badges: end -->
+
+<!-- description: start -->
+
+This is package {finddxtemplate}: Tools to create a Rmd template for
+html/pdf documents.  
+Current version is 0.0.0.9000. <!-- description: end -->
+
+## Installation
+
+<!-- install: start -->
+
+The list of dependencies required to install this package is: magrittr,
+testthat.
+
+To install the package, you can run the following script
+
+``` r
+# install.packages("remotes")
+remotes::install_local(path = "finddxtemplate_0.0.0.9000.tar.gz")
+```
+
+## Doc and coverage
+
+  - pkgdown: <https://connect.thinkr.fr/finddxtemplate-pkgdown-website/>
+  - coverage report:
+    <https://connect.thinkr.fr/finddxtemplate-pkgdown-website/coverage.html>
--- dev/dev_history_connect.R
+++ dev/dev_history_connect.R
@@ -6,31 +6,39 @@
 #   __ Si c'est le pkgdown que vous voulez envoyer
 # `        - Rscript "dev/dev_history_connect.R"`
 
+# Ajouter aussi ces 2 variables d'environnement dans votre .Rprofile local
+#  pour le premier envoi manuel sur Connect
+
 # Un stage complet de CI peut-être (Attention avec les espaces, faire du LINT)
 # Changer master par main si besoin
 
 # connect:
 #   stage: deploy-connect
 #   dependencies:
-#     - integration
+#     - pages
 #   only:
+#     - main
 #     - master
+#     - test-ci
 #   script:
-#     - 'curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "https://forge.thinkr.fr/api/v4/projects/$CI_PROJECT_ID/jobs/artifacts/master/download?job=pages" &&
-#               unzip artifacts.zip &&
-#               rm artifacts.zip &&
-#               echo "copied master artifacts" ||
-#               echo "copied master artifacts failed"'
 #     - Rscript "dev/dev_history_connect.R"
+#   artifacts:
+#     paths:
+#     # This will download artifacts from previous jobs
+#         - public
+#     expire_in: 30 days
 
-# Ne pas oublier d'ajouter le stage "deploy-connect" à la liste des stages du début du CI
+# Ne pas oublier d'ajouter le stage "deploy-connect"
+# à la liste des stages du début du fichier gitlab-ci.yml
+# Dans cet ordre
+
 # stages:
 #   - build
 #   - test
 #   - pkgdown
 #   - pkgdown-move
-#   - deploy-connect
 #   - deploy
+#   - deploy-connect
 
 # /!\ => La premiere fois, executer en commentant le parametre "appID"
 # Les fois suivantes, décommenter "appID" pour mettre le numéro de l'app sur Connect
@@ -39,13 +47,30 @@
 # Définir les droits de lecture de l'app sur Connect avec des individus et pas des groupes.
 # Seuls les devs du projet et le client contact y ont accès
 
+# Changer `mypackage` par le nom de votre package dans ce fichier
+
+# Après le premier envoi manuel, définissez une url sur Connect directement
+# pour avoir accès au site web - Ajuster les droits de lecture aussi
+# Ensuite, mettez à jour le README pour dire où est le rapport de CI - pkgdown et coverage
+# Doc and coverage
+
+# - pkgdown: https://connect.thinkr.fr/finddxtemplate-pkgdown-website/
+# - coverage report: https://connect.thinkr.fr/finddxtemplate-pkgdown-website/coverage.html
+
+
 # Deps
 install.packages("rsconnect")
 
 ## deploy shinyapp
 # orgiwd <- setwd(".") # Uncomment here and below
 ## deploy pkgdown
-origwd <- setwd("docs")
+# pkgdown::build_site()
+if (dir.exists("public")) {
+  origwd <- setwd("public")
+} else {
+  origwd <- setwd("docs")
+}
+print(paste("--", getwd(), "--"))
 
 rsconnect::addServer("https://connect.thinkr.fr/__api__", name = "connect")
 rsconnect::connectApiUser(account = Sys.getenv("CONNECT_USER"),
@@ -57,11 +82,11 @@ appFiles <- appFiles[!grepl(".Rprofile|renv|rstudio_|deliverables|dev|data-raw|d
 
 rsconnect::deployApp(
   ".",                          # the directory containing the content
-  appName = "pkgdown-website",
+  appName = "finddxtemplate-pkgdown-website",
   appFiles = appFiles,          # the list of files to include as dependencies (all of them)
   appPrimaryDoc = "index.html", # the primary file for pkgdown
   # appPrimaryDoc = "app.R", # the primary file for shinyapp # Uncomment here
-  # appId = xx, # Define when known
+  appId = 509, # Define when known
   account = Sys.getenv("CONNECT_USER"),
   server  = "connect",
   forceUpdate = FALSE
