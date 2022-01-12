f3f1f1801a8f5e3e902baaf2b1b3f6f62fd5b085
margot <margot@thinkr.fr>
2022-01-12 10:10:51 +0100

    Move html rendering in a dev_history_script
    Why:
    
    * CI failed
    
    This change addresses the need by:
    
    * Creating a new dev_history file
    
    Issues
    Issue #2

---
+++ dev/dev_history_render_template.R
@@ -0,0 +1,21 @@
+## Script pour deployer sur Connect avec le CI
+
+# Deps
+install.packages("rmarkdown")
+
+## find rmd file
+rmd_file <- file.path(getwd(), "inst", "rmarkdown", "rmd_template.Rmd")
+
+## deploy html
+if (dir.exists("public")) {
+  origwd <- setwd("public")
+} else {
+  origwd <- getwd()
+}
+print(paste("--", getwd(), "--"))
+
+rmarkdown::render(input = rmd_file,
+                  output_format = rmarkdown::html_document(),
+                  output_dir = getwd())
+
+setwd(origwd)
