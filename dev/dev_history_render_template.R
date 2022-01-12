6c1c514195d0afc005fb722d908db3d985a5ea5d
margot <margot@thinkr.fr>
2022-01-12 11:54:28 +0100

    Render html in public for the CI
    Why:
    
    * CI did not found the html file
    
    This change addresses the need by:
    
    * Udating dev_history and gitlabci.yml
    
    Issues
    Issue #8

--- dev/dev_history_render_template.R
+++ dev/dev_history_render_template.R
@@ -3,16 +3,7 @@
 ## find rmd file
 rmd_file <- file.path(getwd(), "inst", "rmarkdown", "rmd_template.Rmd")
 
-## deploy html
-if (dir.exists("public")) {
-  origwd <- setwd("public")
-} else {
-  origwd <- getwd()
-}
-print(paste("--", getwd(), "--"))
-
+## render html
 rmarkdown::render(input = rmd_file,
                   output_format = rmarkdown::html_document(),
-                  output_dir = getwd())
-
-setwd(origwd)
+                  output_dir = "public")
